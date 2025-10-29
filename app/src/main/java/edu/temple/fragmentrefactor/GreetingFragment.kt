69fd5eb1d8e4a3f9c59d274e7a369723a9f515eb
tup86819 <tup86819@temple.edu>
2025-10-29 15:22:25 -0400

    Create GreetingFragment.kt

---
+++ app/src/main/java/edu/temple/fragmentrefactor/GreetingFragment.kt
@@ -0,0 +1,35 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class GreetingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_greeting, container, false)
+
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+
+        return view
+    }
+}
